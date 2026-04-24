547f2e3b920095ebdc163f3a13e5b924944368b6
etierbaquedano <etier.baquedano@inacapmail.cl>
2026-04-23 23:45:37 -0400

    tersera vercion: mejora de html, css y js

---
+++ js/scripts-index-carro.js
@@ -0,0 +1,251 @@
+function aumentar_cantidad() {
+    let inputCantidad = document.getElementById("cantidad");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad() {
+    let inputCantidad = document.getElementById("cantidad");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad2() {
+    let inputCantidad = document.getElementById("cantidad2");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad2() {
+    let inputCantidad = document.getElementById("cantidad2");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad3() {
+    let inputCantidad = document.getElementById("cantidad3");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad3() {
+    let inputCantidad = document.getElementById("cantidad3");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad4() {
+    let inputCantidad = document.getElementById("cantidad4");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad4() {
+    let inputCantidad = document.getElementById("cantidad4");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad5() {
+    let inputCantidad = document.getElementById("cantidad5");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad5() {
+    let inputCantidad = document.getElementById("cantidad5");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad6() {
+    let inputCantidad = document.getElementById("cantidad6");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad6() {
+    let inputCantidad = document.getElementById("cantidad6");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad7() {
+    let inputCantidad = document.getElementById("cantidad7");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad7() {
+    let inputCantidad = document.getElementById("cantidad7");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad8() {
+    let inputCantidad = document.getElementById("cantidad8");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad8() {
+    let inputCantidad = document.getElementById("cantidad8");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad9() {
+    let inputCantidad = document.getElementById("cantidad9");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad9() {
+    let inputCantidad = document.getElementById("cantidad9");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad10() {
+    let inputCantidad = document.getElementById("cantidad10");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad10() {
+    let inputCantidad = document.getElementById("cantidad10");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad11() {
+    let inputCantidad = document.getElementById("cantidad11");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad11() {
+    let inputCantidad = document.getElementById("cantidad11");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad12() {
+    let inputCantidad = document.getElementById("cantidad12");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad12() {
+    let inputCantidad = document.getElementById("cantidad12");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad13() {
+    let inputCantidad = document.getElementById("cantidad13");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad13() {
+    let inputCantidad = document.getElementById("cantidad13");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad14() {
+    let inputCantidad = document.getElementById("cantidad14");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad14() {
+    let inputCantidad = document.getElementById("cantidad14");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad15() {
+    let inputCantidad = document.getElementById("cantidad15");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad15() {
+    let inputCantidad = document.getElementById("cantidad15");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+
+function aumentar_cantidad16() {
+    let inputCantidad = document.getElementById("cantidad16");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad16() {
+    let inputCantidad = document.getElementById("cantidad16");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad17() {
+    let inputCantidad = document.getElementById("cantidad17");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad17() {
+    let inputCantidad = document.getElementById("cantidad17");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad18() {
+    let inputCantidad = document.getElementById("cantidad18");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad18() {
+    let inputCantidad = document.getElementById("cantidad18");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad19() {
+    let inputCantidad = document.getElementById("cantidad19");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad19() {
+    let inputCantidad = document.getElementById("cantidad19");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+function aumentar_cantidad20() {
+    let inputCantidad = document.getElementById("cantidad20");
+    let cantidad = parseInt(inputCantidad.value);
+    inputCantidad.value = cantidad + 1;
+}
+function disminuir_cantidad20() {
+    let inputCantidad = document.getElementById("cantidad20");
+    let cantidad = parseInt(inputCantidad.value);
+    if (cantidad > 1) {
+        inputCantidad.value = cantidad - 1;
+    }
+}
+let producto = JSON.parse(localStorage.getItem("producto"));
+
+if (producto) {
+  document.getElementsByClassName("carro-item")[0].innerHTML = `
+    <h2>${producto.nombre}</h2>
+    <p>Precio unitario: $${producto.precio}</p>
+    <p>Cantidad: ${producto.cantidad}</p>
+    <p>ub Total: $${producto.total}</p>
+  `;
+}
